ed29b3cea86b79a3895af766c3c1894cfcf41fd0
someone19032 <mats.hout@hotmail.com>
2025-06-13 15:40:25 +0200

    Delete test.py
    (this was for testing so im deleting it cuz i didnt add it on purpose)

--- test.py
+++
@@ -1,1 +0,0 @@
-# sorry, dont execute the old one, i forgot to remove this from it, i made this to test my grabber (you can use the 2nd option to make a grabber and this whas to test one i made with it)
